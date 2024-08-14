c5fe9ba3e94cbba28ae58250885b7548c45154ba
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-14 04:57:09 +0000

    Datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,29 @@
+"use strict" ; // treat all code as newer version of javascript
+
+// alert(3 + 3) 
+// we are using nodejs not browser that's why showing alert "undefined"
+
+/*
+Note :-  your main focuse should on code readability, 
+         code readability should be high
+*/
+
+/*  DataTypes in js 
+    (i)String :- anything is double cout ""
+    (ii)numbers :- (0-9) or range 2 power 56
+    (iii)Boolean :- (true & false)
+    (iv)bigInt 
+    (v)doubleInt
+    (vi)null :- is not zero 
+    (vii)undefined :- when variable is not have any value 
+    (viii)symbol :- for using uniqueness 
+    (ix)objects :- like typeof for find the type of variable 
+*/
+
+
+//object
+console.log(typeof("harsh"))
+console.log(typeof(19))
+console.log(typeof null); // is an object 
+console.log(typeof undefined); // is an undefined
+
