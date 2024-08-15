c8961e7e7d38405826ce12058f08f5083f798443
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-15 10:35:36 +0000

    time and date

---
+++ 01_basics/07_Date&time.js
@@ -0,0 +1,28 @@
+const myDate = new Date() // this is date object. at the end of object brackets () , must.
+// console.log(myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// const myCreatedate = new Date(2023, 0, 23, 5, 8)
+// const myCreatedate = new Date(2023, 0, 23)
+// const myCreatedate = new Date("2024-10-11")
+const myCreatedate = new Date("11-10-2001")
+// console.log(myCreatedate.toLocaleDateString());
+// console.log(myCreatedate.toDateString());
+// console.log(myCreatedate.toLocaleString());
+
+
+const myTimestamp = Date.now()
+console.log(myTimestamp); // output in milisecond : 1723718015328
+console.log(myCreatedate.getTime()); // output : 1005350400000
+
+
+// *********** convert into seconds ******************
+
+
+
