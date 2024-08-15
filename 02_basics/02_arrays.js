638fa856b80458860dc2832445d71b6c0076e6dd
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-15 14:27:33 +0000

    array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,10 +1,15 @@
 const marvals = ["ironman", "thor", "superman"]
 const dc_Hero = ["flash", "batman", "spiderman"]
-marvals.push(dc_Hero)
+// marvals.push(dc_Hero)
 // console.log(marvals);
 
 // yha pr jb dono array to marge krenge to dusra array kuch is trh se 
 // push hoga : [ 'ironman', 'thor', 'superman', [ 'flash', 'batman', 'spiderman' ] ]
 
-// how can i access
-console.log(marvals[3][2])
+
+//concat: put all array element in one array 
+//like :[ 'ironman', 'thor', 'superman', 'flash', 'batman', 'spiderman' ]
+const allheros = marvals.concat(dc_Hero)
+console.log(allheros);
+
+// console.log(marvals[3][2])
