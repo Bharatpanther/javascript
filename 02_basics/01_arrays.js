680dd8f4a0c6149c451d326a58d7df14ba89312f
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-15 12:00:37 +0000

    array methods

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+// Array introduction 
+const myArr  = [0,1,2,3,4,5,6,7]
+const myHeros = ["aj", "jk", "hanumanji"]
+
+const myArr2 = new Array(1,2,1,3,8)
+// console.log(myArr2[1]);
+myArr2[5] ="ghanshyam";
+// console.log(myArr2);
+
+//***************  Arrays Methods *******************
+/* 
+   push, pop, ushift, shift, includes, index, join, slice, splice
+*/ 
+
+// const  newAry1 = myArr2.pop() // remove element at the end of array 
+// const  newAry2 = myArr2.push(2) // add element at the end of array 
+// const  newAry3 = myArr2.shift() // remove element at  0th index (at first position) of array 
+// const  newAry4 = myArr2.unshift(1) // add element at  0th index (at first position) of array 
+// const  newAry5 = myArr2.includes(8) //if exists in array then true else false
+// const  newAry6 = myArr2.indexOf(9) // showing element index no if exists else -1 
+// console.log(myArr2);
+// console.log(myAry2);
+
+
+
+
