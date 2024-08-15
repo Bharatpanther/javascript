d66ee859184c1d290e8d462cfb64de6ab1ea17ac
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-15 15:52:40 +0000

    array from,of,isArray

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -11,5 +11,25 @@ const dc_Hero = ["flash", "batman", "spiderman"]
 //like :[ 'ironman', 'thor', 'superman', 'flash', 'batman', 'spiderman' ]
 const allheros = marvals.concat(dc_Hero)
 console.log(allheros);
-
 // console.log(marvals[3][2])
+
+
+// focus on this mehtod 
+const allnewheros = [...marvals, ...dc_Hero] //u can add more than two array by using comma 
+console.log(allnewheros);
+
+const another_arr = [1,2,3,4,[5,6,9],5,[4,5,[5,2]]]
+const real_another_array = another_arr.flat(2)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("hitesh_dangi")) // false means is not an array
+console.log(Array.from("harshvardhan")) // converted into an array 
+
+console.log(Array.from({name : "harsh"})) /* it directly not converted into an array 
+                                you must specified keys of array or value of array */
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3)); // of() work same as from means output is same
