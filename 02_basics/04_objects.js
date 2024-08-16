488d09456c1b8f0f696452fbf1367b777be557b8
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-16 05:10:07 +0000

    object cai or code

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,10 @@
+// const tinderuser =  new object()  // singleton  onject because it's creat using constructor
+
+const Tinderuser =  {}
+Tinderuser.id = 1452
+Tinderuser.name = "harshvardhan"
+Tinderuser.age = 22
+Tinderuser.email = "harsh@gmail.com"
+console.log(Tinderuser);
+// console.log(JSON.stringify(Tinderuser)); // convert object into string
+
