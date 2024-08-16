133976f024eab83d37cc5eccf49f8b1d21566060
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-16 09:13:04 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,40 @@
+// Syntax : function function_name() { }
+
+function saymyName() {  //function defination/ declaration
+    console.log("H");
+    console.log("a");
+    console.log("r");
+    console.log("s");
+    console.log("h");
+    
+}
+
+// saymyName() // function execution
+
+/*
+// program to add two number using function
+function addTwoNum(num1, num2) {  //here num1 and num2 is parameter
+    const total = num1 + num2;
+    console.log(total);
+    
+}
+
+//here it is argument
+addTwoNum(5,9) // output : 14
+addTwoNum(5,"9") // output : 59 typeof string 
+addTwoNum(5,"a")  // output : 5a
+addTwoNum(5,null) // output : 5
+
+*/
+
+
+function addTwoNum(num1, num2) {
+    let result = num1 + num2
+    console.log("Harshvardhan");
+    // return result;
+    return num1 + num2
+    // console.log("harsh"); // after return statement, any block of code doen't execute 
+}
+
+const result = addTwoNum(5,6)
+console.log("Result : ", result);
