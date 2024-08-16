82f44e5110c2f4c321828fe21546e26305d86d79
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-16 04:47:10 +0000

    freeze in object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,15 +5,44 @@
 // const jsUser  = { } //called empty object
 
 const jsUser = { name : "hitesh",
+                 "full name" : "Harsh Samariya", //"full name" ok dot(.) se access nhi kr skte h 
                  age : 22,
-
+                 location : "jaipur",
+                 email : "hitest@gmailc.om",
+                 isLoggedIn : false,
+                 lastLoggedIn : ["monday", "tuesday"]
 }
 
 
 //in object key consider as string but not written like this "name": "Harsh" x |  name : "harsh" ✓
 
 // 1st method of access object element
-console.log(jsUser.name); // for access objects properties use . operator before key
+// console.log(jsUser.name); // for access objects properties use . operator before key
 
-// 2nd method (you can access more than one properties in this ) 
-console.log(jsUser["name", "age"]);
+// 2nd method  
+// console.log(jsUser["name", "age", "full name", "email", "location"]);
+// console.log(jsUser["full name"]);
+
+
+// change email value of object 
+//1.  jsUser["age"] = 125 
+//2.  jsUser.email = "pawan@gimmin.com"
+// console.log(jsUser);
+
+
+
+//how to freeze object (means prevent changes in values)
+// Object.freeze(jsUser)
+// jsUser.email = "pawan@gimmin.com"
+// console.log(jsUser); 
+
+jsUser.greeting = function() {
+    console.log("hello world good morning");
+}
+jsUser.greetingTwo = function() {
+    console.log(`hello ${this.name} good morning`);
+}
+
+// console.log(jsUser.greeting); // outuput : Function (anonymous)]
+console.log(jsUser.greeting()); // outuput :hello world good morning
+console.log(jsUser.greetingTwo()); // outuput :hello hitesh good morning
