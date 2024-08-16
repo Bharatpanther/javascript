fc0f5fbc2f654f336049f5099afb932c617643dd
Bharatpanther <150528649+Bharatpanther@users.noreply.github.com>
2024-08-16 13:14:14 +0000

    function with object and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -51,5 +51,51 @@ function loginuserName(username = "Hari") {
     }
     return `${username} just logged in `
 }
-console.log(loginuserName("kamal Thakur"))
+// console.log(loginuserName("kamal Thakur"))
+
+
+
+//  *********************** Functon with objects and arrays**************************
+/*function calculateCartPrice(...num1) { // here we can use rest operator (...)
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500)); //output : [ 200, 400, 500 ]
+*/
+
+/*
+function calculateCartPrice(val1,val2,...num1) { // here we can use rest operator (...)
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000)); // output : [ 500, 2000 ]
+// here first two values take val1 and val2, ramaining values goes in num1 variable
+
+*/
+
+// passing an object in function
+const user = {
+    name :"hitest",
+    price  : 199
+}
+
+function handleObject(anyobject) {
+    console.log(`username is ${anyobject.name} & price is ${anyobject.price}`);   
+}
+
+// handleObject(user) // here you can pass array name
+handleObject({       // here you can update array value
+    name : "harsh",
+    price : 599
+})
+
+//passing an array in function
+const myArray = [100,200,300,400]
+
+function returnArray(getarray) {
+    return getarray[1]
+}
+
+// console.log(returnArray(myArray));        // here you can pass array name
+console.log(returnArray([250,350]));
 
