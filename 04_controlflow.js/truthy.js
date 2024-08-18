79c222b141182e0fff7440cc81d170db29531799
Harshvardhan <150528649+Bharatpanther@users.noreply.github.com>
2024-08-18 13:53:00 +0000

    truthy

---
+++ 04_controlflow.js/truthy.js
@@ -0,0 +1,17 @@
+// thruthy means assume thing is true (assume kara jata h true hi hoga)
+const userEmail = "harsh@gmail.com"
+if (userEmail){
+    console.log("Got user email");
+}else{
+    console.log("Don't have user email"); 
+
+}
+
+
+//truthy values 
+// true ," " (string with space), "0", 'false',[],{},functiopn(){}
+
+//falsy values
+// false, 0, -0, ""(empty string), null, undefined, NaN, 0n(bigInt)
+
+
