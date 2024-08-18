5c660dbd0adcddcbcdbc638f3fbd204f70717ab4
Harshvardhan <150528649+Bharatpanther@users.noreply.github.com>
2024-08-18 12:28:16 +0000

    control statement

--- 04_controlflow.js/one.js
+++ 04_controlflow.js/one.js
@@ -41,3 +41,52 @@ console.log(`power is out of scope ${power}`);
 */
 
 
+//implicit scope
+/*
+const balance = 1000
+if(balance >500) console.log("balance is greater than 500");
+
+//use ,  to add multipleline line in this scope. don't forget about semicolon at the end
+if(balance >500) console.log("balance is greater than 500"),console.log("harshvardhan");
+*/
+
+
+/*
+// nested if-else
+ const balance = 1000
+ if (balance <500){
+    console.log("lessthen 500");
+    
+ }else if(balance < 750){
+    console.log("lessthan 750");
+    
+ }else if(balance >900){
+    console.log("lessthen 900");
+    
+ }else {
+    console.log("equals to hundread");
+    
+ }
+*/
+
+
+const userloggesIn = true
+const debitcard = true
+const googleloggedin = false
+const emailloggedin = true
+
+// && gives output when both are condition is true 
+if (userloggesIn && debitcard){
+    console.log("allow to buy course");
+    
+}
+
+// || or gives output when any one condtiton is true out of multiple condition
+if (googleloggedin || emailloggedin){
+    console.log("user logged in");
+    
+}
+
+
+
+ 
