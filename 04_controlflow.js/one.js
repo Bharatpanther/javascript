0f893a0eae28cfaf7e0aafe2a67027691ac068ce
Harshvardhan <150528649+Bharatpanther@users.noreply.github.com>
2024-08-18 11:47:21 +0000

    controlflow

---
+++ 04_controlflow.js/one.js
@@ -0,0 +1,43 @@
+// if statement
+/*syntex
+if (condition){
+
+}
+*/
+
+//<,>,>=,<=,==,!=,===
+
+
+/*
+const isuserloggedIn = true
+
+if (2 != 3){
+    console.log("executed"); 
+}
+
+// if-else
+const temprature = 41
+if(temprature === 41){
+    console.log("temprature is lessthan 50");
+    
+}else{
+    console.log("greater than 50");
+    
+}
+*/
+
+
+
+
+/*
+const score = 200
+if (score > 100){
+    let power = "fly"
+    console.log(`power is : ${power}`);
+    
+}
+
+console.log(`power is out of scope ${power}`);
+*/
+
+
