4c06d53723b9b2089305137463e59769ef5c1f25
Harshvardhan <150528649+Bharatpanther@users.noreply.github.com>
2024-08-18 14:25:41 +0000

    true-false

--- 04_controlflow.js/truthy.js
+++ 04_controlflow.js/truthy.js
@@ -14,4 +14,38 @@ if (userEmail){
 //falsy values
 // false, 0, -0, ""(empty string), null, undefined, NaN, 0n(bigInt)
 
+//how to check empty array
+if(userEmail.length === 0){
+    console.log("Array is empty");
+    
+}
+
+//how to check empty object 
+
+const emptyObj = {}
+if (Object.keys(emptyObj).length === 0){ //object.key(emptyObj) is return an array. so after that apply array method of previou code
+    console.log("Object is empty");
+    
+}
+
+
+
+//Nullish coalesing operator (??) is type of logical operator
+val1 = 10 ?? 15 //output 10
+val1 = null ?? 10 //output 10
+val1 = undefined ?? 15  //output 15
+val1 = null ?? 10 ?? 15 //output 10 
+
+
+//Terniary operator
+// syntax : condition ? true : false
+
+//example 1 
+const iceCream =80
+iceCream >= 80 ? console.log("pay for iceCream"):console.log("don't pay for iceCream");
+
+
+
+//example 2
+(10<1) ?console.log("harsh") : console.log("Hitesh");
 
