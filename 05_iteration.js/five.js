ffe0cb960d5eaec79b5bc45dbcef09f8625e4959
Harshvardhan <150528649+Bharatpanther@users.noreply.github.com>
2024-08-19 15:39:20 +0000

    for Each

--- 05_iteration.js/five.js
+++ 05_iteration.js/five.js
@@ -1,1 +1,57 @@
 //for each loop
+const coding =["js","ruby","python","reactjs"]
+/*
+coding.forEach( function(val){
+    console.log(val);
+    
+} )
+*/
+
+
+
+//using arrow function 
+/*
+coding.forEach( (item) => {
+    console.log(item);
+} )
+*/
+
+
+/*
+function printme(item){
+    console.log(item);
+    
+}
+coding.forEach(printme)
+*/
+
+
+
+
+coding.forEach((ele, index, arr) => {
+    console.log(ele, index, arr);
+    
+});
+
+
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "c++",
+        languageFileName : "cpp"
+    },
+    {
+        languageName : "pyhton",
+        languageFileName : "py"
+    }
+]
+
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    
+})
