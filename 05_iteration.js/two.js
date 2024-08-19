bc886880fa15d6f79a3e6d5fdebb0548ef5aa142
Harshvardhan <150528649+Bharatpanther@users.noreply.github.com>
2024-08-19 06:23:56 +0000

    while and do while

---
+++ 05_iteration.js/two.js
@@ -0,0 +1,33 @@
+// while
+/*
+syntax : while(condtion){
+    code 
+    increment/decrement condition
+} 
+*/
+
+
+let index = 1
+while(index <=10){
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ['ghanshyam','rajesh','kundra','pawan']
+let arr = 0
+while(arr < myArray.length){
+    // console.log(myArray[arr]);
+    arr++
+}
+
+
+
+// do while 
+
+let score = 1
+
+do {
+    console.log(`my score is ${score}`);
+    score++    
+} while (score <=0 );
