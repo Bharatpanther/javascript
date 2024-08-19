942c269d55c5e05988b81da7cd4dbfbb960b2f9b
Harshvardhan <150528649+Bharatpanther@users.noreply.github.com>
2024-08-19 06:52:34 +0000

    map anf for of

---
+++ 05_iteration.js/three.js
@@ -0,0 +1,55 @@
+// higher order loops in js
+/*
+["","","",""]
+[{},{},{},{}]
+
+*/
+
+//for of loop
+
+const arr =[1,2,3,4,5,6]
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+
+const greetings = "hello, today is Rakshabandhan"
+for (const greet of greetings) {
+    // console.log(greet);
+    
+}
+
+
+
+//maps
+
+const map = new Map()
+map.set('Rj', 'Rajasthan')
+map.set('Gj', 'Gujrat')
+map.set('Mp', 'Madhyapradesh')
+map.set('Hr', 'Haryana')
+// console.log(map);
+
+
+// how to apply for of loop in map 
+for (const key of map) {
+    console.log(key);   
+    
+}
+/*
+//  it's give an array 
+[ 'Rj', 'Rajasthan' ]
+[ 'Gj', 'Gujrat' ]
+[ 'Mp', 'Madhyapradesh' ]
+[ 'Hr', 'Haryana' ]
+*/
+
+
+
+for (const [key,value] of map) {
+    console.log(key , ':-' , value);
+    
+    
+}
