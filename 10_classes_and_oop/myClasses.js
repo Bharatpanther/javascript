4b89a40241b8b60481cb30ca794cd9543c961f89
Harshvardhan <150528649+Bharatpanther@users.noreply.github.com>
2024-09-02 05:08:36 +0000

    class

--- 10_classes_and_oop/myClasses.js
+++ 10_classes_and_oop/myClasses.js
@@ -12,11 +12,29 @@ class user{
     }
 
     changeUsername(){
-        return ``
+        return `${this.username.toUpperCase()}`
     }
 }
 
 const chai = new user("Harsh", "chai@gmail.com", 145874)
 console.log(chai);
 console.log(chai.encryptPass());
+console.log(chai.changeUsername());
+
+
+//Behind the scene
+
+function User(username,email,password){
+    this.usrname = username; 
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPass = function(){
+    return `${this.password}abc`
+}
+
+const tea = new User("tea","tea@gmail.com", "12356")
+console.log(tea);
+console.log(tea.encryptPass());
 
